f442821b47d59523bf29c365557847747cac5e42
Oscar Bonduelle <oscar.bonduelle@hotmail.fr>
2020-04-16 23:28:42 +0200

    Update Delete and ShowPointRunnable

--- src/main/java/mrstraw/titlearea/commands/ShowPointRunnable.java
+++ src/main/java/mrstraw/titlearea/commands/ShowPointRunnable.java
@@ -27,16 +27,16 @@ public class ShowPointRunnable implements Runnable {
         for(Double theta=Math.random()*2*Math.PI/32; theta < 2 * Math.PI ; theta += 2*Math.PI/32){
             for(Double phi = Math.random()*2*Math.PI/32 ; phi < 2 * Math.PI ; phi += 2*Math.PI/32){
 
-                world.spawnParticle(Particle.FLAME
+                world.spawnParticle(Particle.SMOKE_LARGE
                         ,location.getX()+distance*Math.cos(theta)*Math.cos(phi)
                         ,location.getY()+distance*Math.sin(theta)
                         ,location.getZ()+distance*Math.cos(theta)*Math.sin(phi)
-                        ,1,0,0,0,0);
-                world.spawnParticle(Particle.CRIT_MAGIC
+                        ,1,0,0,0);
+                world.spawnParticle(Particle.FIREWORKS_SPARK
                         ,location.getX()+radius*Math.cos(theta)*Math.cos(phi)
                         ,location.getY()+radius*Math.sin(theta)
                         ,location.getZ()+radius*Math.cos(theta)*Math.sin(phi)
-                        ,1,0,0,0,0);
+                        ,1,0,0,0);
                 //Bukkit.broadcastMessage(Particle.FLAME+"Location: "+location);
             }
         }
